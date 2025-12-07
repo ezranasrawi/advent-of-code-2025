93138b55d21a9f38f4f31ea37655ee00fb8b002e
ezranasrawi <ezranasrawi@gmail.com>
2025-12-07 21:18:44 +0100

    Solve day 5

--- solutions/day-05.py
+++ solutions/day-05.py
@@ -1,6 +1,6 @@
 from _getinput import getinput
 
-data = getinput('05', True)
+data = getinput('05', False)
 
 ranges, nums = [], []
 
@@ -23,4 +23,18 @@ for num in nums:
     
 print(f"Part 1: {fresh}")
 
-print(ranges)
+sorted_ranges = sorted(ranges)
+merged_ranges = []
+
+for start, end in sorted_ranges:
+    if not merged_ranges or merged_ranges[-1][1] < start:
+        merged_ranges.append((start, end))
+    else:
+        s, e = merged_ranges[-1]
+        merged_ranges[-1] = (s, max(e, end))
+
+total_covered = 0
+for start, end in merged_ranges:
+    total_covered += end - start + 1
+
+print(f"Part 2: {total_covered}")
