de01dc17a8e385b87b53465d45d7ed27df97aab5
Rose Nasrawi <r.nasrawi11@gmail.com>
2025-12-09 19:10:30 +0100

    Solve day 6 ugly

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -4,12 +4,51 @@ data = getinput('06', True)
 
 data = [line.split() for line in data]
 data = list(map(list, zip(*data)))
+ops, nums = [], []
+
+for i, line in enumerate(data):
+    ops.append(line.pop(-1))
+    nums.append(line)
 
 total = 0
 
+for i, num in enumerate(nums):
+    op = ops[i]
+    total += eval(op.join(num))
+
+print('Part 1', total)
+
+def parse_r2l(num):
+    new = []
+
+    while num:
+        last = [n[-1] for n in num]
+        num = [n[:-1] for n in num if n[:-1]]
+        new.append(''.join(last))
+
+    return new
+
+data = getinput('06', False)
+data = list(map(list, zip(*data)))
+
+num = []
+total = 0
+
 for line in data:
-    op = line.pop(-1) 
-    total += eval(op.join(line))
+    line = ''.join(line)
+    
+    if line[-1] in ['*', '+']:
+        op = line[-1]
+        n = line[:-1]
+        num.append(n.strip())
+    
+    if line.strip().isdigit():
+        n = line.strip()
+        num.append(n)
 
-print(total)
+    if line.isspace() or line == data[-1]:
+        total += eval(op.join(num))
+        num = []
 
+total+= eval(op.join(num))
+print('Part 2', total)
